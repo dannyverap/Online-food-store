5b8bb9b7a5e79787a16d69da77fdd7ea78c60c8a
Danny Vera Palomino <danny.verap@gmail.com>
2024-07-31 11:09:05 +0200

    Create IngredientsItem class with ingredient category and restaurant association

---
+++ src/main/java/com/danny/Online/food/store/model/IngredientsItem.java
@@ -0,0 +1,28 @@
+package com.danny.Online.food.store.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientsItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private IngredientCategory ingredientCategory;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
+
+    private boolean inStock = true;
+}

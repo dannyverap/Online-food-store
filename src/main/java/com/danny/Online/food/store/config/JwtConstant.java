d3984a2d42adcf3fd67bbad68ea99f452be482b9
Danny Vera Palomino <danny.verap@gmail.com>
2024-07-31 17:31:15 +0200

    Create JwtConstants

---
+++ src/main/java/com/danny/Online/food/store/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.danny.Online.food.store.config;
+
+//Todo: use environment variables
+public class JwtConstant {
+    public static final String SECRET_KEY = "SecretKeyToGenJWTs";
+    public static final String JWT_HEADER = "Authorization";
+}

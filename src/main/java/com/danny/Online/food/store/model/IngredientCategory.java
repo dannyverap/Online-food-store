d24bc14efe3077b0f4fdbcb3d49ea5c31f695ee1
Danny Vera Palomino <danny.verap@gmail.com>
2024-07-31 11:10:05 +0200

    Create IngredientCategory model to categorize ingredients by restaurant

---
+++ src/main/java/com/danny/Online/food/store/model/IngredientCategory.java
@@ -0,0 +1,29 @@
+package com.danny.Online.food.store.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "ingredientCategory", cascade = CascadeType.ALL)
+    private List<IngredientsItem> ingredientsItems = new ArrayList<>();
+}

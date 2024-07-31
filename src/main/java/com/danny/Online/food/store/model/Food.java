e516a63acc78907952396944fa66fc098eff0e1d
Danny Vera Palomino <danny.verap@gmail.com>
2024-07-31 11:06:51 +0200

    Create Food entity with category, images, and availability flags

---
+++ src/main/java/com/danny/Online/food/store/model/Food.java
@@ -0,0 +1,46 @@
+package com.danny.Online.food.store.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Long price;
+
+    @ManyToOne
+    private FoodCategory foodCategory;
+
+    @Column(length = 1000)
+    @ElementCollection
+    private List<String> images;
+
+    private boolean available;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private boolean isVegetarian;
+
+    private boolean isSeasonal;
+
+    @ManyToOne
+    private IngredientsItem ingredientsItem;
+
+    private Date creationDate;
+}

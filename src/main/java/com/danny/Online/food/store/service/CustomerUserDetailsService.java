de085c14fe898190101915f9db952a5618d28a33
Danny Vera Palomino <danny.verap@gmail.com>
2024-07-31 17:41:35 +0200

    Implement UserDetailsService for User Authentication
    Added CustomerUserDetailsService to load user details for authentication.
    Implemented UserDetailsService to retrieve user by email.
    Converted user role to GrantedAuthority for access control.
    Integrated with Spring Security to provide user information during authentication.

---
+++ src/main/java/com/danny/Online/food/store/service/CustomerUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.danny.Online.food.store.service;
+
+import com.danny.Online.food.store.model.USER_ROLE;
+import com.danny.Online.food.store.model.User;
+import com.danny.Online.food.store.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email " + email);
+        }
+
+        USER_ROLE role = user.getRole();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.toString()));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}

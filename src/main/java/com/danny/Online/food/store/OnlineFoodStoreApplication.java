97399f764a5ae2190cac1542ccdfabd7741a01c3
Danny Vera Palomino <danny.verap@gmail.com>
2024-07-31 09:39:48 +0200

    Initial commit

---
+++ src/main/java/com/danny/Online/food/store/OnlineFoodStoreApplication.java
@@ -0,0 +1,13 @@
+package com.danny.Online.food.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danny/Online/food/store/OnlineFoodStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.Online.food.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

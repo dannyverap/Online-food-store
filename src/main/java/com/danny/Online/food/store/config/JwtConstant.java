4bf7d6a77a39b0cf6da8d5772af4a60f8f558b55
Danny Vera Palomino <danny.verap@gmail.com>
2024-08-07 12:37:03 +0200

    Update SECRET_KEY

--- src/main/java/com/danny/Online/food/store/config/JwtConstant.java
+++ src/main/java/com/danny/Online/food/store/config/JwtConstant.java
@@ -2,6 +2,7 @@ package com.danny.Online.food.store.config;
 
 //Todo: use environment variables
 public class JwtConstant {
-    public static final String SECRET_KEY = "SecretKeyToGenJWTs";
+    public static final String SECRET_KEY = "SecretKeyToGenJWTasdnsjandjnsajkdnjsandjansdjnn@@#1ndjsndjsnajdnjass";
     public static final String JWT_HEADER = "Authorization";
+    public static final String EXPIRATION_TIME = "86400000";
 }

0fe527a0f995ce330c8357d2938ef72cc535f64e
Danny Vera Palomino <danny.verap@gmail.com>
2024-08-07 12:41:13 +0200

    Add AuthController with endpoints for user signup and login
    - Implemented POST /auth/signup for user registration, using AuthUserService to handle signup logic.
    - Implemented POST /auth/signin for user login, utilizing CustomerUserDetailsService and AuthUserService for authentication.
    - Added handling for creating users and authenticating users with JWT response.

---
+++ src/main/java/com/danny/Online/food/store/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.danny.Online.food.store.controller;
+
+import com.danny.Online.food.store.model.User;
+import com.danny.Online.food.store.request.LoginRequest;
+import com.danny.Online.food.store.response.AuthResponse;
+import com.danny.Online.food.store.service.AuthUserService;
+import com.danny.Online.food.store.service.CustomerUserDetailsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+    private final AuthUserService authUserService;
+    private final CustomerUserDetailsService customerUserDetailsService;
+
+    public AuthController(AuthUserService authUserService, CustomerUserDetailsService customerUserDetailsService) {
+        this.authUserService = authUserService;
+        this.customerUserDetailsService = customerUserDetailsService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
+        AuthResponse authResponse = authUserService.SignUp(user);
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> loginUserHandler(@RequestBody LoginRequest user) throws Exception {
+        UserDetails authentication = customerUserDetailsService.loadUserByUsername(user.getEmail());
+        AuthResponse authResponse = authUserService.SignIn(authentication, user.getPassword());
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+}

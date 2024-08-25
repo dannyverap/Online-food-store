13e32a992ac682ffa3295c10ff6d540ba9851c23
Danny Vera Palomino <danny.verap@gmail.com>
2024-08-25 10:39:32 +0200

    Create RestaurantService implementation

---
+++ src/main/java/com/danny/Online/food/store/service/RestaurantServiceImpl.java
@@ -0,0 +1,127 @@
+package com.danny.Online.food.store.service;
+
+import com.danny.Online.food.store.dto.RestaurantDto;
+import com.danny.Online.food.store.model.Address;
+import com.danny.Online.food.store.model.Restaurant;
+import com.danny.Online.food.store.model.User;
+import com.danny.Online.food.store.repository.AddressRepository;
+import com.danny.Online.food.store.repository.RestaurantRepository;
+import com.danny.Online.food.store.repository.UserRepository;
+import com.danny.Online.food.store.request.CreateRestaurantRequest;
+import com.danny.Online.food.store.request.UpdateRestaurantRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RestaurantServiceImpl implements RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final AddressRepository addressRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository, AddressRepository addressRepository, UserRepository userRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.addressRepository = addressRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Restaurant createRestaurant(CreateRestaurantRequest request, User user) throws Exception {
+        Address address = addressRepository.save(request.getAddress());
+        Restaurant restaurant = new Restaurant();
+        restaurant.setAddress(address);
+        restaurant.setContactInformation(request.getContactInformation());
+        restaurant.setName(request.getName());
+        restaurant.setOwner(user);
+        restaurant.setCuisineType(request.getCuisineType());
+        restaurant.setDescription(request.getDescription());
+        restaurant.setImages(request.getImages());
+        restaurant.setOpeningHours(request.getOpeningHours());
+        restaurant.setCreationDate(LocalDateTime.now());
+        return restaurantRepository.save(restaurant);
+
+    }
+
+    @Override
+    public Restaurant updateRestaurant(UpdateRestaurantRequest updateRestaurant, Long restaurantId) throws Exception {
+        Restaurant restaurant = getRestaurantById(restaurantId);
+
+        if (updateRestaurant.getCuisineType() != null) {
+            restaurant.setCuisineType(updateRestaurant.getCuisineType());
+        }
+        if (updateRestaurant.getDescription() != null) {
+            restaurant.setDescription(updateRestaurant.getDescription());
+        }
+        if (updateRestaurant.getName() != null) {
+            restaurant.setName(updateRestaurant.getName());
+        }
+        return restaurantRepository.save(restaurant);
+    }
+
+    @Override
+    public void deleteRestaurant(Long restaurantId) throws Exception {
+        Restaurant restaurant = getRestaurantById(restaurantId);
+        restaurantRepository.delete(restaurant);
+    }
+
+    @Override
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    @Override
+    public List<Restaurant> searchRestaurants(String query) {
+        return restaurantRepository.findBySearchQuery(query);
+    }
+
+    @Override
+    public Restaurant getRestaurantById(Long restaurantId) throws Exception {
+        Optional<Restaurant> restaurant = restaurantRepository.findById(restaurantId);
+        if (restaurant.isEmpty()) {
+            throw new Exception("Restaurant not found");
+        }
+        return restaurant.get();
+    }
+
+
+    @Override
+    public Restaurant getRestaurantByUserId(Long userId) throws Exception {
+        Restaurant restaurant = restaurantRepository.findByOwnerId(userId);
+        if (restaurant == null) {
+            throw new Exception("Restaurant not found with owner id: " + userId);
+        }
+        return restaurant;
+    }
+
+    @Override
+    public RestaurantDto toggleFavouriteStatus(Long restaurantId, User user) throws Exception {
+        Restaurant restaurant = getRestaurantById(restaurantId);
+
+        RestaurantDto restaurantDto = new RestaurantDto();
+        restaurantDto.setDescription(restaurant.getDescription());
+        restaurantDto.setImages(restaurant.getImages());
+        restaurantDto.setTitle(restaurant.getName());
+        restaurantDto.setImages(restaurant.getImages());
+
+        if (user.getFavorites().contains(restaurantDto)) {
+            user.getFavorites().remove(restaurantDto);
+        } else {
+            user.getFavorites().add(restaurantDto);
+        }
+        userRepository.save(user);
+
+        return restaurantDto;
+    }
+
+    @Override
+    public Restaurant updateRestaurantStatus(Long restaurantId) throws Exception {
+        Restaurant restaurant = getRestaurantById(restaurantId);
+        restaurant.setOpen(!restaurant.isOpen());
+        return restaurantRepository.save(restaurant);
+    }
+}
